480b9c8b61df84d618930993a179121343a1f8b6
Cami <Cami@WINDOWS-L673FIJ>
2016-08-28 11:50:11 -0300

    Se sube el proyecto inicial del Despachador

---
+++ Despachador/src/main/java/com/fing/DespachadorApplication.java
@@ -0,0 +1,12 @@
+package com.fing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DespachadorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DespachadorApplication.class, args);
+	}
+}
