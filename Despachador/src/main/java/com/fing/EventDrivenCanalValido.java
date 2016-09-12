facce4e457b2ba62639109df3531e04714f2ad92
Cami <Cami@192.168.254.1>
2016-09-11 21:22:33 -0300

    Subo la clase EventDrivenCanalValido

---
+++ Despachador/src/main/java/com/fing/EventDrivenCanalValido.java
@@ -0,0 +1,54 @@
+package com.fing;
+
+import javax.jms.Destination;
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.MessageConsumer;
+import javax.jms.MessageListener;
+import javax.jms.Session;
+import javax.jms.TextMessage;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.integration.handler.MessageProcessor;
+
+public class EventDrivenCanalValido implements MessageListener {
+	
+	private Destination destination;
+	private Session session;
+	private Log log = LogFactory.getLog(MessageProcessor.class);
+	
+	public EventDrivenCanalValido(Destination destino, Session sesion)
+	{
+		this.destination = destino;
+		this.session = sesion;
+	}
+
+	public void CrearConsumidor() {
+		try {
+			MessageConsumer consumer = session.createConsumer(destination);
+			consumer.setMessageListener(this);
+		} catch (JMSException e1) {
+			e1.printStackTrace();
+		}
+	}
+	
+	@Override
+	public void onMessage(Message message) {
+		TextMessage msg = (TextMessage) message;
+		try {
+			String value = msg.getText();
+			System.out.println("mensaje del canal valido " + value);
+			ApplicationContext context = new ClassPathXmlApplicationContext("Bean.xml");				
+			EnviarColaLocal endpoint = (EnviarColaLocal) context.getBean("entrada", EnviarColaLocal.class);
+			endpoint.EnviarCola(value, "ColaTranslator");
+			
+		} catch (JMSException e) {
+			log.error("Error processing message", e);
+
+		}		
+	}
+
+}
