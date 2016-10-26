1e18bb2a729263786a7a65e860220c699104f01b
javier <javier@javier-PC>
2016-10-26 19:21:10 -0300

    Consulta Estado Reserva

--- TicketInco/src/main/java/com/fing/ticketinco/ReservaEntradas.java
+++ TicketInco/src/main/java/com/fing/ticketinco/ReservaEntradas.java
@@ -5,6 +5,7 @@
  */
 package com.fing.ticketinco;
 
+import java.util.List;
 import org.apache.log4j.Logger;
 import javax.jws.WebService;
 import javax.jws.WebMethod;
@@ -22,10 +23,24 @@ public class ReservaEntradas {
      * Web service operation
      */
     @WebMethod(operationName = "reserva_de_entradas")
-    public long reserva_de_entradas(@WebParam(name = "identificador_evento") int identificador_evento, @WebParam(name = "fechaevento") String fechaevento) {
+    public long reserva_de_entradas(@WebParam(name = "identificador_evento") int identificador_evento, @WebParam(name = "fechaevento") String fechaevento, @WebParam(name = "Listadehorarios") Horario[] listahorarios ) {
         //TODO write your implementation code here:
         fgen.info ("Identificador del evento" + identificador_evento);
         fgen.info ("Fecha del evento" + fechaevento);
+        
+        for (int i = 0; i < listahorarios.length; i++) {
+        
+             fgen.info ("Horario : " + listahorarios[i].getHorario().toString());
+             
+             List<Disponibilidad> listadisponibles = listahorarios[i].disponibilidades;
+             for (int j = 0; j < listadisponibles.size() ; j++) {                
+                fgen.info ("   Disponibilidad - Cantidad: " + listadisponibles.get(j).getCantidad());
+                fgen.info ("   Disponibilidad - Precio: " + listadisponibles.get(j).getPrecio());
+                fgen.info ("   Disponibilidad - Sector: " + listadisponibles.get(j).getSector());
+             }           
+             
+             
+        }
         
         return 0;
     }
