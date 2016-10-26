f7c90cebe73041b8639a06f5e943b9cd1470298f
javier <javier@javier-PC>
2016-10-26 19:55:19 -0300

    Reserva de entradas busco el id del evento

--- TicketInco/src/main/java/com/fing/ticketinco/ReservaEntradas.java
+++ TicketInco/src/main/java/com/fing/ticketinco/ReservaEntradas.java
@@ -5,6 +5,8 @@
  */
 package com.fing.ticketinco;
 
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import org.apache.log4j.Logger;
 import javax.jws.WebService;
@@ -23,7 +25,7 @@ public class ReservaEntradas {
      * Web service operation
      */
     @WebMethod(operationName = "reserva_de_entradas")
-    public long reserva_de_entradas(@WebParam(name = "identificador_evento") int identificador_evento, @WebParam(name = "fechaevento") String fechaevento, @WebParam(name = "Listadehorarios") Horario[] listahorarios ) {
+    public long reserva_de_entradas(@WebParam(name = "identificador_evento") int identificador_evento, @WebParam(name = "fechaevento") Date fechaevento, @WebParam(name = "Listadehorarios") Horario[] listahorarios ) {
         //TODO write your implementation code here:
         fgen.info ("Identificador del evento" + identificador_evento);
         fgen.info ("Fecha del evento" + fechaevento);
@@ -40,6 +42,37 @@ public class ReservaEntradas {
              }           
              
              
+        }         
+        // busco el evento y que la lista de horarios sea en la que quiero reservar
+        ListaEventos eventos = new ListaEventos();
+        Evento e = eventos.buscarEvento(identificador_evento, fechaevento);
+        List<Horario> horariosRetornar = new ArrayList<Horario>();
+        if(e != null)
+        {
+            horariosRetornar = e.getHoarios();
+        }     
+        
+        if (horariosRetornar != null)
+        {
+            for (int i = 0; i < horariosRetornar.size(); i++) {
+                for (int j = 0; j < listahorarios.length; j++) {
+                    if (horariosRetornar.get(i).horario==listahorarios[j].horario)
+                    {   for (int k = 0; k < horariosRetornar.get(i).disponibilidades.size(); k++) {
+                            for (int l = 0; l < listahorarios[j].disponibilidades.size(); l++) {
+                                Disponibilidad d= horariosRetornar.get(i).disponibilidades.get(k);
+                                Disponibilidad r= listahorarios[j].disponibilidades.get(l);
+                                if  (d.cantidad >= r.cantidad && d.sector== r.sector)
+                                {
+                                    // genero el identificador de reserva para retornar
+                                    return 1;
+                                }
+                                
+                            }
+                            
+                        }
+                    }
+                }
+            }
         }
         
         return 0;
