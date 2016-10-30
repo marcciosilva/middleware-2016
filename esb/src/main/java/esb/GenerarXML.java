d6ad169a4ff4557fc78691aa10dd064d6300a053
WINDOWS-L673FIJ\Cami <camiroji@gmail.com>
2016-10-30 19:45:50 -0300

    Nueva version esb

--- esb/src/main/java/esb/GenerarXML.java
+++ esb/src/main/java/esb/GenerarXML.java
@@ -21,60 +21,33 @@ public class GenerarXML {
 
 			DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
 			DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
-
-			// root elements
+			
 			Document doc = docBuilder.newDocument();
 			Element rootElement = doc.createElement("confirmacion");
 			doc.appendChild(rootElement);
 
-			// staff elements
-			//Element staff = doc.createElement("Staff");
-			//rootElement.appendChild(staff);
-
-			// set attribute to staff element
-			//Attr attr = doc.createAttribute("id");
-			//attr.setValue("1");
-			//staff.setAttributeNode(attr);
-
-			// shorten way
-			// staff.setAttribute("id", "1");
-
-			// firstname elements
 			Element nroTarjetaElement = doc.createElement("nroTarjeta");
 			nroTarjetaElement.appendChild(doc.createTextNode(nroTarjeta));
 			rootElement.appendChild(nroTarjetaElement);
-
-			// lastname elements
+			
 			Element fechaVencimientoElement = doc.createElement("fechaVencimiento");
 			fechaVencimientoElement.appendChild(doc.createTextNode(fechaVencimiento));
 			rootElement.appendChild(fechaVencimientoElement);
-
-			// nickname elements
+			
 			Element digitoVerificadorElement = doc.createElement("digitoVerificador");
 			digitoVerificadorElement.appendChild(doc.createTextNode(digitoVerificador));
 			rootElement.appendChild(digitoVerificadorElement);
-
-			// salary elements
+			
 			Element montoElement = doc.createElement("monto");
 			montoElement.appendChild(doc.createTextNode(monto));
 			rootElement.appendChild(montoElement);
-
-			// write the content into xml file
+			
 			TransformerFactory transformerFactory = TransformerFactory.newInstance();
 			Transformer transformer = transformerFactory.newTransformer();
-			DOMSource source = new DOMSource(doc);
+			DOMSource source = new DOMSource(doc);			
 			
-			//System.out.println(source.toString());
 			return source;
-			//StreamResult result = new StreamResult(new File("C:\\file.xml"));
-
-			// Output to console for testing
-			// StreamResult result = new StreamResult(System.out);
-
-			//transformer.transform(source, result);
-
-			//System.out.println("File saved!");
-
+			
 		  } catch (ParserConfigurationException pce) {
 			pce.printStackTrace();
 		  } catch (TransformerException tfe) {
@@ -83,4 +56,33 @@ public class GenerarXML {
 		return null;
 		}
 	
+	public DOMSource GenerarXMLAnular(String idConfirmacionPago)
+	{
+		try {
+
+			DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
+			DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
+			
+			Document doc = docBuilder.newDocument();
+			Element rootElement = doc.createElement("anulacion");
+			doc.appendChild(rootElement);
+
+			Element idConfirmacionPagoElement = doc.createElement("idConfirmacionPago");
+			idConfirmacionPagoElement.appendChild(doc.createTextNode(idConfirmacionPago));
+			rootElement.appendChild(idConfirmacionPagoElement);	
+			
+			TransformerFactory transformerFactory = TransformerFactory.newInstance();
+			Transformer transformer = transformerFactory.newTransformer();
+			DOMSource source = new DOMSource(doc);			
+			
+			return source;
+			
+		  } catch (ParserConfigurationException pce) {
+			pce.printStackTrace();
+		  } catch (TransformerException tfe) {
+			tfe.printStackTrace();
+		  }
+		return null;
+		}
+
 }
