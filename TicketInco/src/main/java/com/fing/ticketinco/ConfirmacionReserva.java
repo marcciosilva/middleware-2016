05c741c78576205dcd3c02e6f3ea7357e4a2ab1e
Marccio Silva <marccio.silva@fing.edu.uy>
2016-11-08 00:12:49 -0300

    Fix sobre la ruta de imagenes.

--- TicketInco/src/main/java/com/fing/ticketinco/ConfirmacionReserva.java
+++ TicketInco/src/main/java/com/fing/ticketinco/ConfirmacionReserva.java
@@ -5,40 +5,19 @@
  */
 package com.fing.ticketinco;
 
-
-
+import com.fing.ws.MedioPagoLocal;
+import com.fing.ws.MedioPagoLocalService;
 import java.awt.Image;
 import java.io.BufferedInputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.net.URL;
-import java.text.ParseException;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.logging.Level;
-import javax.imageio.ImageIO;
-import javax.jws.WebService;
-import javax.jws.WebMethod;
-import javax.jws.WebParam;
-import javax.xml.namespace.QName;
-import javax.xml.ws.BindingProvider;
-import javax.xml.ws.Service;
-import javax.xml.ws.soap.Addressing;
-import javax.xml.ws.soap.MTOMFeature;
-import javax.xml.ws.soap.SOAPBinding;
-
-
-import com.fing.ws.MedioPagoLocal;
-import com.fing.ws.MedioPagoLocalService;
 import java.io.Serializable;
-
 import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.logging.Level;
 import javax.jws.WebMethod;
 import javax.jws.WebParam;
 import javax.jws.WebService;
@@ -47,17 +26,13 @@ import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.Entity;
 import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.MediaType;
-
-
-import org.apache.log4j.Logger;
-
-
 import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.ws.soap.Addressing;
+import javax.xml.ws.soap.MTOMFeature;
 import org.apache.log4j.Logger;
 import ws.callback.NotificarConfirmacionReserva;
 import ws.callback.NotificarConfirmacionReserva_Service;
 import ws.callback.ReservasRetornar;
-
 
 /**
  *
@@ -69,8 +44,8 @@ import ws.callback.ReservasRetornar;
 public class ConfirmacionReserva {
 
 	final static Logger fgen = Logger.getLogger(ConfirmacionReserva.class);
-//	final static java.util.logging.Logger logger = java.util.logging.Logger.
-//			getLogger(ConfirmacionReserva.class.getName());
+	final static java.util.logging.Logger logger = java.util.logging.Logger.
+			getLogger(ConfirmacionReserva.class.getName());
 
 	/**
 	 * This is a sample web service operation
@@ -109,30 +84,30 @@ public class ConfirmacionReserva {
 		//TODO: Hay que agregar WS-Addressing y WS-Security
 		ListaReservas listaReservas = new ListaReservas();
 		Reserva reserva = listaReservas.buscarReserva(idReserva);
-                ReservasRetornar entradasRetornar = new ReservasRetornar();
+		ReservasRetornar entradasRetornar = new ReservasRetornar();
 		if (reserva != null) {
-			if(reserva.Estado == 2)
-                {
-                    //entradasRetornar.descripcion = "La reserva ya fue confirmada";
-                    //entradasRetornar.i = idReserva;                    
-                    //return entradasRetornar;
-                }
-                else if(reserva.Estado == 0)
-                {
-                    //entradasRetornar.descripcion = "La reserva se encuentra en estado cancelado";
-                    //entradasRetornar.idReserva = idReserva;                    
-                    //return entradasRetornar;                    
-                }
-                
-                
+			if (reserva.Estado == 2) {
+				//entradasRetornar.descripcion = "La reserva ya fue confirmada";
+				//entradasRetornar.i = idReserva;
+				//return entradasRetornar;
+			} else if (reserva.Estado == 0) {
+				//entradasRetornar.descripcion = "La reserva se encuentra en estado cancelado";
+				//entradasRetornar.idReserva = idReserva;
+				//return entradasRetornar;
+			}
+
 			double monto = calcularMonto(reserva);
 			if (idMedioPago == 1000) {
 				try {
-					MedioPagoLocalService medioPagoService = new MedioPagoLocalService();
-					MedioPagoLocal medioPagoLocal = medioPagoService.getMedioPagoLocalPort();
-					String digitoVerificadorStr = String.valueOf(digitoVerificador);
+					MedioPagoLocalService medioPagoService
+							= new MedioPagoLocalService();
+					MedioPagoLocal medioPagoLocal = medioPagoService.
+							getMedioPagoLocalPort();
+					String digitoVerificadorStr = String.valueOf(
+							digitoVerificador);
 					String montoStr = String.valueOf(monto);
-					medioPagoLocal.confirmarPago(nroTarjeta, fechaVencimiento.toString(), digitoVerificadorStr, montoStr);
+					medioPagoLocal.confirmarPago(nroTarjeta, fechaVencimiento.
+							toString(), digitoVerificadorStr, montoStr);
 					reserva.Estado = 2;
 					Pago pago = new Pago();
 					pago.reservas.add(reserva);
@@ -171,8 +146,8 @@ public class ConfirmacionReserva {
 					} else {
 						//return "PagosYa! dice: " + msg;
 					}
-				} catch (Exception e) {					
-				
+				} catch (Exception e) {
+
 					fgen.info(e.getMessage());
 					//return e.getMessage();
 
@@ -180,52 +155,54 @@ public class ConfirmacionReserva {
 			} else {
 				//return "El medio de pago no existe";
 			}
-                        //Generar entradas en formato binario para enviar a las notificaciones
-                //URL url = new URL("\\resources\\Entradas\\cinemaTicket.jpg");
-                //Image imgEntrada = ImageIO.read(new File("C:\\Users\\Cami\\Documents\\Fing\\Middleware\\Obligatorio2\\ImagenesEntradas\\cinemaTicket.jpg"));
-                String pathFile = "C:\\Users\\Cami\\Documents\\Fing\\Middleware\\Obligatorio2\\ImagenesEntradas\\cinemaTicket.jpg";
-                ArrayList<Image> imagenesEntradas = new ArrayList<Image>();
-                //imagenesEntradas.add(imgEntrada);
-                //enable MTOM in client
-                
-                NotificarConfirmacionReserva_Service service = new NotificarConfirmacionReserva_Service();
-                NotificarConfirmacionReserva port = service.getNotificarConfirmacionReservaPort(new MTOMFeature(true));
-                //NotificacionConfirmacionReservaEntrada notificacionWS = new NotificacionConfirmacionReservaEntrada();
-                //BindingProvider bp = (BindingProvider) port;
-                //SOAPBinding binding = (SOAPBinding) bp.getBinding();
-                //binding.setMTOMEnabled(true);
-                byte[] imagenBinaria = obtenerByteImagen(pathFile);
-                ReservasRetornar retornarNotificacion = port.notificarEntradas(idReserva, imagenBinaria);
-                byte[] imagenRet = retornarNotificacion.getImagenEntrada();
-                reserva.Estado = 2;
-                                
-                //entradasRetornar.idReserva = idReserva;
-                //entradasRetornar.respuesta = "Imagenes procesadas con exito";
-                
-                //return null;
-                return imagenRet;
+			//Generar entradas en formato binario para enviar a las notificaciones
+			//URL url = new URL("\\resources\\Entradas\\cinemaTicket.jpg");
+			//Image imgEntrada = ImageIO.read(new File("C:\\Users\\Cami\\Documents\\Fing\\Middleware\\Obligatorio2\\ImagenesEntradas\\cinemaTicket.jpg"));
+//                String pathFile = "C:\\Users\\Cami\\Documents\\Fing\\Middleware\\Obligatorio2\\ImagenesEntradas\\cinemaTicket.jpg";
+			String pathFile = getClass().getClassLoader().getResource(
+					"resources/Entradas/cinemaTicket.jpg").getPath();
+			ArrayList<Image> imagenesEntradas = new ArrayList<Image>();
+			//imagenesEntradas.add(imgEntrada);
+			//enable MTOM in client
+
+			NotificarConfirmacionReserva_Service service
+					= new NotificarConfirmacionReserva_Service();
+			NotificarConfirmacionReserva port = service.
+					getNotificarConfirmacionReservaPort(new MTOMFeature(true));
+			//NotificacionConfirmacionReservaEntrada notificacionWS = new NotificacionConfirmacionReservaEntrada();
+			//BindingProvider bp = (BindingProvider) port;
+			//SOAPBinding binding = (SOAPBinding) bp.getBinding();
+			//binding.setMTOMEnabled(true);
+			byte[] imagenBinaria = obtenerByteImagen(pathFile);
+			ReservasRetornar retornarNotificacion = port.notificarEntradas(
+					idReserva, imagenBinaria);
+			byte[] imagenRet = retornarNotificacion.getImagenEntrada();
+			reserva.Estado = 2;
+
+			//entradasRetornar.idReserva = idReserva;
+			//entradasRetornar.respuesta = "Imagenes procesadas con exito";
+			//return null;
+			return imagenRet;
 		}
 		//return entradasRetornar;
-                return null;
-	}  
+		return null;
+	}
 
-        private byte[] obtenerByteImagen(String filePath)
-    {
-        try {
-            File file = new File(filePath);
-            FileInputStream fis = new FileInputStream(file);
-            BufferedInputStream inputStream = new BufferedInputStream(fis);
-            byte[] fileBytes = new byte[(int) file.length()];
-            inputStream.read(fileBytes);
-            inputStream.close();
-            return fileBytes;
-        } catch (IOException ex) {
-            java.util.logging.Logger.getLogger(ConfirmacionReserva.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        return null;
-    }
-
-		
+	private byte[] obtenerByteImagen(String filePath) {
+		try {
+			File file = new File(filePath);
+			FileInputStream fis = new FileInputStream(file);
+			BufferedInputStream inputStream = new BufferedInputStream(fis);
+			byte[] fileBytes = new byte[(int) file.length()];
+			inputStream.read(fileBytes);
+			inputStream.close();
+			return fileBytes;
+		} catch (IOException ex) {
+			java.util.logging.Logger.getLogger(ConfirmacionReserva.class.
+					getName()).log(Level.SEVERE, null, ex);
+		}
+		return null;
+	}
 
 	@XmlRootElement(name = "pago")
 	private class PagoPagosYa implements Serializable {
@@ -248,20 +225,17 @@ public class ConfirmacionReserva {
 
 	}
 
-        private double calcularMonto(Reserva r)
-        {
-            double monto = 0;
-            ArrayList<Horario> horarios = r.horarios;
-            
-            for(Horario h: horarios)
-            {
-                ArrayList<Disponibilidad> disponibilidades = h.disponibilidades;
-                for(Disponibilidad d : disponibilidades)
-                {
-                    monto = monto + (d.cantidad * d.precio);
-                }                
-            }
-            return monto;
-        }
+	private double calcularMonto(Reserva r) {
+		double monto = 0;
+		ArrayList<Horario> horarios = r.horarios;
+
+		for (Horario h : horarios) {
+			ArrayList<Disponibilidad> disponibilidades = h.disponibilidades;
+			for (Disponibilidad d : disponibilidades) {
+				monto = monto + (d.cantidad * d.precio);
+			}
+		}
+		return monto;
+	}
 
 }
