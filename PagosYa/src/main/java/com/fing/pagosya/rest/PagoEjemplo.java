0a7f4d64ca9788f429230e4183e01cb1e20429b2
Marccio Silva <marccio.silva@fing.edu.uy>
2016-11-10 15:35:20 -0300

    PagosYa pasa a ser un proyecto de Maven.

---
+++ PagosYa/src/main/java/com/fing/pagosya/rest/PagoEjemplo.java
@@ -0,0 +1,72 @@
+/*
+ * Copyright 2016 marccio.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.fing.pagosya.rest;
+
+import com.fing.pagosya.dtos.Pago;
+import java.util.Calendar;
+import java.util.Locale;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriInfo;
+
+/**
+ * REST Web Service
+ *
+ * @author marccio
+ */
+@Path("pagoEjemplo")
+public class PagoEjemplo {
+
+	@Context
+	private UriInfo context;
+
+	/**
+	 * Creates a new instance of TestResource
+	 */
+	public PagoEjemplo() {
+	}
+
+	/**
+	 * Retrieves representation of an instance of
+ com.fing.pagosya.dtos.PagoEjemplo
+	 *
+	 * @return an instance of java.lang.String
+	 */
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response getXml() {
+		Pago pago = new Pago(1234, Calendar.getInstance(Locale.FRENCH), 1,
+				1000.1);
+		return Response.status(201).entity(pago).build();
+	}
+
+	/**
+	 * PUT method for updating or creating an instance of PagoEjemplo
+	 *
+	 * @param content representation for the resource
+	 * @return an HTTP response with content of the updated or created resource.
+	 */
+	@PUT
+	@Consumes("application/xml")
+	public void putXml(String content) {
+	}
+}
---
+++ PagosYa/src/main/webapp/index.jsp
@@ -0,0 +1,10 @@
+<html>
+	<body>
+		<h2>API REST de PagosYa</h2>
+		<h4>Recursos disponibles:</h4>
+		<ul>
+			<li>recursos/confirmacionPago</li>
+			<li>recursos/pagoEjemplo (el GET devuelve un pago de ejemplo en JSON)</li>
+		</ul>
+	</body>
+</html>
